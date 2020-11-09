e418d7d21afc3480d0a5667ccc2384a73cf96f8b
Vladimir Sokolov <vsokol74@main.ru>
2020-11-09 18:16:06 +0300

    Spring boot rest full service with AOP

---
+++ SpringBootRESTfulWebServiceWithAOP/src/main/java/store/sokolov/springboot/GreetingController.java
@@ -0,0 +1,18 @@
+package store.sokolov.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    final private static String template = "Hello, %s!";
+    final private AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
